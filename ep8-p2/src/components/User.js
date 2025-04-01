9857eaf20e5b286128626784864f218655bd3938
Ajaybazilissac <ajaybazilissac93@gmail.com>
2025-04-01 22:25:57 +0530

    ep8-p2

--- ep8-p2/src/components/User.js
+++ ep8-p2/src/components/User.js
@@ -3,11 +3,10 @@ const User = ({ name }) => {
   const [count] = useState(0);
   return (
     <div className="user-card">
-      <h1>Count=</h1>
-      <h1>Count={count}</h1>
+      <h1>Count = {count}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: Kerala</h3>
-      <h3>Contact2 : ajaybazil93@gmail.com</h3>
+      <h3>Contact : ajaybazil93@gmail.com</h3>
     </div>
   );
 };
--- ep8-p2/src/components/UserClass.js
+++ ep8-p2/src/components/UserClass.js
@@ -4,13 +4,16 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      count2: 0,
     };
   }
   render() {
     const { name, location, contact } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
-        <h1>Count={this.state.count}</h1>
+        <h1>Count: {count}</h1>
+        <h3>Count 2: {count2}</h3>
         <h2>Name:{name} </h2>
         <h3>Location: {location}</h3>
         <h3>Contact : {contact}</h3>
