b00364bfd358d2c968d56e720ad7a65abd0a9c98
Ajaybazilissac <ajaybazilissac93@gmail.com>
2025-04-13 11:51:27 +0530

    ep9-p1

---
+++ ep9-p1/src/app.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+
+// AppLayout Component
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={appRouter} />
+//   </React.StrictMode>
+// );
