58a064d467052e65bb4f31869362f73d1f31e7e7
Ajaybazilissac <ajaybazilissac93@gmail.com>
2025-05-01 12:01:19 +0530

    ep11-p2

---
+++ ep11-p2/src/components/RestaurantMenu.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
+const RestaurantMenu = () => {
+  //const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
+  const resInfo = useRestaurantMenu(resId);
+
+  if (resInfo === null) return <Shimmer />;
+  const { name, cuisines, costForTwo, costForTwoMessage } =
+    resInfo.cards[2].card.card.info;
+
+  const { itemCards } =
+    resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
+
+  // console.log("itemcard", resInfo);
+  // console.log(
+  //   "title--",
+  //   resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.title
+  // );
+  const categories =
+    resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+
+  return (
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines.join(",")} -- {costForTwoMessage}
+      </p>
+      {/* categories accordions */}
+      {categories.map((category) => (
+        <RestaurantCategory
+          key={category?.card?.card.title}
+          data={category?.card?.card}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RestaurantMenu;
